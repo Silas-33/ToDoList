775b277238f71e3f60505edacba3ee85b0e1e0b0
Silas <silas.pego@aluno.senai.br>
2026-08-13 11:09:14 -0300

    Alterações 13/08

--- src/todolist/ToDoList.java
+++ src/todolist/ToDoList.java
@@ -4,6 +4,8 @@
  */
 package todolist;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Aluno
@@ -14,7 +16,28 @@ public class ToDoList {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner (System.in);
+        Boolean continuar = true;
+        while (continuar) {
+            System.out.println("\n=== LISTA DE TAREFAS ===");
+            System.out.println("1 - Adicionar tarefa");
+            System.out.println("2 - Listar tarefas");
+            System.out.println("3 - Concluir tarefa");
+            System.out.println("4 - Excluir tarefa");
+            System.out.println("5 - Sair");
+            
+            System.out.println("Selecione uma opção: ");
+            
+            int opcao = scanner.nextInt();
+            
+            if(opcao == 1){
+                
+                
+                
+            }else if(opcao == 2){
+                
+            }
+        }
+        
     }
-    
 }
